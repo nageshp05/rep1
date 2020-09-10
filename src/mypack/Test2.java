9b6634e8ef8b52b17d96c8fec84d48e7d1d2c998
nageshp05 <66491477+nageshp05@users.noreply.github.com>
2020-09-10 23:30:24 +0400

    Update Test2.java

--- src/mypack/Test2.java
+++ src/mypack/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hi... Initial entry missing");
-		System.out.println("Hence, code modified");
+		System.out.println("Hence, code modified for pull");
 	
 	}
 }
